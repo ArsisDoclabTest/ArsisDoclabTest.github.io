70e2c7f6849bf8d5c0900d304f576b11e0995e07
Olga <novikova@reg.ru>
2014-05-27 07:25:19 +0400

    images, main page

--- gulpfile.js
+++ gulpfile.js
@@ -11,13 +11,13 @@ gulp.task('bower', function() {
 });
 
 gulp.task('jquery', ['bower'], function() {
-  	gulp.src(['bower_components/jquery/jquery.js', 
-  			  'bower_components/jquery-ui-touch-punch/jquery.ui.touch-punch.js', 
+  	gulp.src(['bower_components/jquery/jquery.js',
   			  'bower_components/jquery-ui/ui/jquery.ui.core.js', 
   			  'bower_components/jquery-ui/ui/jquery.ui.widget.js',
   			  'bower_components/jquery-ui/ui/jquery.ui.mouse.js',
   			  'bower_components/jquery-ui/ui/jquery.ui.position.js',
-  			  'bower_components/jquery-ui/ui/jquery.ui.draggable.js'])
+  			  'bower_components/jquery-ui/ui/jquery.ui.draggable.js',
+  			  'bower_components/jquery-ui-touch-punch/jquery.ui.touch-punch.js'])
 	  	.pipe(concat('all-jquery.js'))
 	    .pipe(uglify())
 	    .pipe(gulp.dest('root/javascript'))
@@ -30,11 +30,21 @@ gulp.task('less', function() {
 	    .pipe(gulp.dest('root/stylesheets'))
 });
 
+gulp.task('images', function() {
+    gulp.src('assets/images/*')
+	    .pipe(gulp.dest('root/images'))
+});
+
+gulp.task('closure', ['bower'], function() {
+    gulp.src('bower_components/closure-library/**')
+	    .pipe(gulp.dest('root/javascript/closure-library'))
+});
+
 gulp.task('css', function() {
-    gulp.src(['assets/stylesheets/pygment_trac.css', 'assets/stylesheets/stylesheets.css'])
+    gulp.src(['assets/stylesheets/pygment_trac.css', 'assets/stylesheets/stylesheet.css'])
 	  	.pipe(concat('page.css'))
 	    .pipe(cssminify())
 	    .pipe(gulp.dest('root/stylesheets'))
 });
 
-gulp.task('default', ['css', 'less', 'jquery']);
+gulp.task('default', ['css', 'less', 'images', 'closure', 'jquery']);
